5916e41779bdc88c52a4abfb796ee4f35b1e039b
Jesus Angel <jesusres56@gmail.com>
2026-02-26 13:45:31 -0600

    segruidad index

--- index.php
+++ index.php
@@ -119,7 +119,7 @@ $conn = null;
     <meta charset="UTF-8">
     <title>Dashboard | TÁCTICA 8</title>
     <link rel="stylesheet" href="../src/estilos/estilos.css">
-   
+   <script src="../src/js/seguridad.js" defer></script>
     <style>
         /* Mantener estilos originales del dashboard.css */
         
